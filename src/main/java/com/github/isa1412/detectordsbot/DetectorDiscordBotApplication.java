ccc5fc5b89ab1cbb3b73a2face1c8d0d74ad29a4
Alexey Strelchuk <veilmist619@gmail.com>
2022-04-11 16:35:13 +0300

    DDSB-0: added SpringBoot skeleton to the project

---
+++ src/main/java/com/github/isa1412/detectordsbot/DetectorDiscordBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.isa1412.detectordsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectorDiscordBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetectorDiscordBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/isa1412/detectordsbot/DetectorDiscordBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.isa1412.detectordsbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetectorDiscordBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
